69b7e40bec8d51ba7442e2d21753e1aa08e3093b
Karan Mehta <79548116+karanm645@users.noreply.github.com>
2022-08-23 12:47:51 -0500

    first commit

---
+++ intro_to_c#/basic_calculator.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Metrics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Giraffe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a number ");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter another number ");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(num1 + num2);
+        }
+    }
+}
