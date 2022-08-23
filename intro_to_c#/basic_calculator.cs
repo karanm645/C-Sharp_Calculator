891cab9701d955ce62b2076b9b64b1f186ca941d
Karan Mehta <79548116+karanm645@users.noreply.github.com>
2022-08-23 12:51:07 -0500

    Change to double

--- intro_to_c#/basic_calculator.cs
+++ intro_to_c#/basic_calculator.cs
@@ -12,9 +12,9 @@ namespace Giraffe
         static void Main(string[] args)
         {
             Console.Write("Enter a number ");
-            int num1 = Convert.ToInt32(Console.ReadLine());
+            int double = Convert.ToDouble(Console.ReadLine());
             Console.Write("Enter another number ");
-            int num2 = Convert.ToInt32(Console.ReadLine());
+            int double = Convert.ToDouble(Console.ReadLine());
             Console.WriteLine(num1 + num2);
         }
     }
